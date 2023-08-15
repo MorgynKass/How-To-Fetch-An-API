93c57d81636abad33ef4c43e04c8100c7bb4a7af
MorgynKass <morgynkass@gmail.com>
2023-08-15 17:09:29 -0600

    including the screenshot and info about the script

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+
+//Creating a variable named "URL" and setting it to a string with a url. 
 const URL = "https://iskarr.github.io/austindonovan.github.io/api/business.json";
 
+//Using the "fetch()" method to extract the JSON body content from the url.
 fetch(URL)
     .then((response) => response.json())
     .then((jsObject) => {
